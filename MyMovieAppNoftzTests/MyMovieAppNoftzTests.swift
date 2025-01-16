d08d69d48743c9bb6f50df6a68e9e78c96e2c75d
esnoftz <esnoftz@gmail.com>
2025-01-16 07:05:33 -0600

    Initial Commit

---
+++ MyMovieAppNoftzTests/MyMovieAppNoftzTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyMovieAppNoftzTests.swift
+//  MyMovieAppNoftzTests
+//
+//  Created by EVANGELINE NOFTZ on 1/16/25.
+//
+
+import Testing
+@testable import MyMovieAppNoftz
+
+struct MyMovieAppNoftzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
